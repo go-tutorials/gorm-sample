e49cceaec383ac6804bab4732fc218b22140d948
A Ri <ari@tma.com.vn>
2022-03-09 09:36:48 +0700

    update patch function with using Gorm

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"context"
+	"reflect"
+
+	q "github.com/core-go/sql"
 	"gorm.io/gorm"
 
 	. "go-service/internal/model"
@@ -47,8 +50,11 @@ func (r *userRepository) Update(ctx context.Context, user *User) (int64, error) 
 }
 
 func (r *userRepository) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
+	userType := reflect.TypeOf(User{})
+	jsonColumnMap := q.MakeJsonColumnMap(userType)
+	colMap := q.JSONToColumns(user, jsonColumnMap)
 	var userModel User
-	res := r.DB.Model(&userModel).Where("id = ?", user["id"]).Updates(user)
+	res := r.DB.Model(&userModel).Where("id = ?", user["id"]).Updates(colMap)
 	return res.RowsAffected, nil
 }
 
